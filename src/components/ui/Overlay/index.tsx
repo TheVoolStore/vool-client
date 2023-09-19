3637ef251b604d97d2c95762f656fabd1d1ed508
sheyitawn <randombuzzer234@gmail.com>
2023-09-20 00:44:05 +0100

    Improved responsiveness

--- src/components/ui/Overlay/index.tsx
+++ src/components/ui/Overlay/index.tsx
@@ -17,7 +17,7 @@ export const Overlay: React.FC<Props> = ({ text, isOpen, onClose }) => {
       <div className="fixed inset-0 backdrop-blur z-[1000]">
         <div className="opacity-60 fixed inset-0 bg-gradient-to-r from-orange-300 via-orange-500 to-orange-50"></div>
         <div className="fixed inset-0 flex flex-col justify-center items-center">
-          <div className="bg-[#eee7df] px-8 py-14 z-[101] w-[35%] opacity-100 h-[75%]">
+          <div className="bg-[#eee7df] px-8 py-14 z-[101] w-full sm:w-[80%] md:w-[60%] lg:w-[35%] opacity-100 h-[75%]">
             <div className="flex items-center justify-between pb-7">
               <h3 className="text-black text-2xl">{text}</h3>
               <button
